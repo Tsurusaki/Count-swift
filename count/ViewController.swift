d10d4075b045b127083f90c5880a210bef9d5dd0
Tsurusaki <ka.trsk@gmail.com>
2015-03-01 17:03:02 +0900

    swiftのcountです

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!
+    var number:Int = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,61 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func plus(){
+        number = number + 1
+        label.text = "\(number)"
+        self.changeLabelRedColor()
+        self.changeLabelBlueColor()
+    }
+    
+    
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = "\(number)"
+        self.changeLabelRedColor()
+        self.changeLabelBlueColor()
 
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = "\(number)"
+        self.changeLabelRedColor()
+        self.changeLabelBlueColor()
 
+    }
+
+    @IBAction func kakeru(){
+        number = number * number
+        label.text = "\(number)"
+        self.changeLabelRedColor()
+        self.changeLabelBlueColor()
+
+    }
+
+    @IBAction func waru(){
+        number = number / number
+        label.text = "\(number)"
+        self.changeLabelRedColor()
+        self.changeLabelBlueColor()
+
+    }
+    
+    
+    func changeLabelRedColor(){
+        if number >= 10{
+            label.textColor = UIColor.redColor()
+        }
+    }
+    func changeLabelBlueColor(){
+        if number <= -10{
+            label.textColor = UIColor.blueColor()
+        }
+    }
+    
+    
+    
+    
 }
 
